105ca86b3addd317cc4b7f0b18c828503012870c
Mohamedgamal343j <momo.gamal33@gmail.com>
2025-08-02 14:20:31 +0300

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let navbar = document.querySelector('.navbar');
+    let menuBtn = document.querySelector('#menu-btn');
+
+    menuBtn.onclick = () => {
+        navbar.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    };
+});
